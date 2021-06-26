0e37ec36e51a999f84db600a3931d388d9dced15
Liu233w <gh@liu233w.com>
2021-06-27 02:22:08 +1000

    Reproduce the error

---
+++ Migrations/20210626162110_Upgrade.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LongtextIssue.Migrations
+{
+    public partial class Upgrade : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LongString",
+                table: "Entities",
+                type: "varchar(5000)",
+                maxLength: 5000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4(5000)",
+                oldMaxLength: 5000,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LongString",
+                table: "Entities",
+                type: "longtext CHARACTER SET utf8mb4(5000)",
+                maxLength: 5000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(5000)",
+                oldMaxLength: 5000,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
