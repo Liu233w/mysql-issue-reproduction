40c67570ac4155507c75420bde1e8ff3de140b15
Liu233w <gh@liu233w.com>
2021-06-28 00:30:12 +1000

    Explicitly set column type and migrate

--- Migrations/20210627142950_ChangeColumnType.Designer.cs
+++ Migrations/20210627142950_ChangeColumnType.Designer.cs
@@ -2,14 +2,16 @@
 using LongtextIssue;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LongtextIssue.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210627142950_ChangeColumnType")]
+    partial class ChangeColumnType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210627142950_ChangeColumnType.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LongtextIssue.Migrations
+{
+    public partial class ChangeColumnType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LongString",
+                table: "Entities",
+                type: "varchar(5000)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldMaxLength: 5000,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LongString",
+                table: "Entities",
+                type: "longtext",
+                maxLength: 5000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(5000)",
+                oldNullable: true);
+        }
+    }
+}
