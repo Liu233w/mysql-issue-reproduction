d2c6631cbaa89c5b9fc34060726f80dc4ce06a6d
Liu233w <gh@liu233w.com>
2021-06-28 00:39:23 +1000

    Explicitly set column type as longtext

--- Entity.cs
+++ Entity.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LongtextIssue
 {
@@ -6,7 +7,7 @@ namespace LongtextIssue
     {
         public int Id { get; set; }
         
-        [StringLength(5000)]
+        [Column(TypeName = "longtext")]
         public string LongString { get; set; }
     }
 }
